02062398bb1911e546f760f0503b87ce2ceb806e
nto-msx <jgsnto.sd@gmail.com>
2022-09-18 10:15:17 -0300

    Code: C1_W7

---
+++ C1/W7/C1_W7 (1).m
@@ -0,0 +1,37 @@
+%For loop
+% total = 0;
+% 
+% for i=0:3
+%     total = total + i;
+% end
+% 
+% fprintf("%d",total);
+
+% %While function
+% total = 0;
+% limit = 20;
+% 
+% while total < limit
+%     total = total +1;
+% end
+% 
+% fprintf("%d", total);
+
+%Break Statement 
+
+% for i=1:1:11
+%     if i > 5
+%         fprintf("Break\n");
+%         break;
+%     else
+%         continue
+%     end
+% end
+
+%Logical index
+%[1 2 3 4 5] > [4 5 6 7 8]
+
+% Timing a function
+tic; sum(1:1e3); toc
+
+
